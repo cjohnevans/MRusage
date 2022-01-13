1b066327bc09ba1d94149ba9abd70f186fd75f94
sapje1 <evansj31@cardiff.ac.uk>
2022-01-13 16:41:34 +0000

    cvs load working, first plot

--- mrusage.py
+++ mrusage.py
@@ -2,6 +2,7 @@
 # next - plotting of summary data...
 
 import csv
+import matplotlib.pyplot as plt
 
 fname = 'mri_activity_dec_2021.csv'
 
@@ -58,7 +59,7 @@ class Resource:
                 self.repeat_type.append(row['repeat_type'])
                 self.start_date.append(row['start_date'])
                 self.finish_date.append(row['finish_date'])
-                self.duration_minutes.append(row['duration_minutes'])
+                self.duration_minutes.append(int(row['duration_minutes']))
                 self.booking_description.append(row['booking_description'])
                 self.cancellation_date.append(row['cancellation_date'])
                 self.cancellation_notice_length_days.append(row['cancellation_notice_length_days'])
@@ -66,12 +67,36 @@ class Resource:
                 self.cancellation_reason.append(row['project'])
                 self.cancellation_comments.append(row['project'])
 
+                self.date_yymmdd = []
+
+    def calc_datetime(self):
+        # take date part and split y, m, d
+        for ii in self.start_date:
+            datetmp = ii[0:10].split('-') # string
+            datetmp2 = []
+            for jj in datetmp:
+                datetmp2.append(int(jj))
+            print datetmp
+            print type(datetmp)
+            self.date_yymmdd.append(datetmp2)
+        
     def show_bookings(self):
-        print self.project
+        print self.project[2]
+        print self.start_date
+        print self.duration_minutes
+        print self.date_yymmdd
+
+        fig1 = plt.figure()
+        ax1 = fig1.add_subplot(1,1,1)
+        print(range(0, len(self.duration_minutes)))
+        ax1.plot(range(0, len(self.duration_minutes)) , self.duration_minutes )
+        plt.show()
+
 
 dec_bookings = Booking(fname)
 dec_bookings.read_csv()
 
 prisma_west = Resource('3TW')
 prisma_west.get_bookings(dec_bookings.booking_dict)
+prisma_west.calc_datetime()
 prisma_west.show_bookings()
