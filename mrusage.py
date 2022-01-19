8ca90c7bdd347c8d1ffb6655c7c366dd3a6d5f6d
sapje1 <evansj31@cardiff.ac.uk>
2022-01-19 15:48:56 +0000

    dict implmenentation for approved/cancelled plots for all scanners

--- mrusage.py
+++ mrusage.py
@@ -1,4 +1,4 @@
-# analysis of MR usage
+
 # next - plotting of summary data...
 
 import csv
@@ -11,7 +11,7 @@ fname = 'mri_activity_aprnov_2021.csv'
 
 
 # class for a single input csv file (could be several, one month each)
-class Booking:
+class BookingSource:
     def __init__ (self, filename) :
         # lists, with values from each booking
         self.booking_dict = []
@@ -25,11 +25,11 @@ class Booking:
             for row in csvread:
                 self.booking_dict.append(row)
 
-# needs to take the resulting dict, from possible multiple csv files, but resource
-# and generate a resource specific dict.
-class Resource:
-    def __init__ (self, resource_name):
-        self.resource_name = resource_name
+# object containing all bookings for a given 
+class BookingFilter:
+    def __init__ (self, resource_name_filter, booking_status_filter):
+        self.resource_name_filter = resource_name_filter
+        self.booking_status_filter = booking_status_filter
         self.resource = []
         self.project = []
         self.booking_status = []
@@ -50,50 +50,53 @@ class Resource:
         self.cancellation_comments = []
         self.booking_datetime = []  # list of datetime objects
         self.booking_week_num = []
-
+ 
 # get the resource specific bookings
     def get_bookings(self, booking_dict):
         tmp_duration_hours = [] #list
         for row in booking_dict:
-            if row['resource'] == self.resource_name:
-                self.start_date.append(row['start_date'])
-                self.project.append(row['project'])
-                self.booking_status.append(row['booking_status'])
-                self.booker.append(row['booker'])
-                self.owner.append(row['owner'])
-                self.project_type.append(row['project_type'])
-                self.booking_type.append(row['booking_type'])
-                self.repeat_type.append(row['repeat_type'])
-                self.finish_date.append(row['finish_date'])
-                self.duration_minutes.append(row['duration_minutes']) # str
-                tmp_duration_hours.append(float(row['duration_minutes'])/60.0) #list
-                self.booking_description.append(row['booking_description'])
-                self.cancellation_date.append(row['cancellation_date'])
-                self.cancellation_notice_length_days.append(row['cancellation_notice_length_days'])
-                self.cancellation_notice_length_verbose.append(row['cancellation_notice_length_verbose'])
-                self.cancellation_reason.append(row['project'])
-                self.cancellation_comments.append(row['project'])
+            if row['resource'] == self.resource_name_filter:
+                if row['booking_status'] == self.booking_status_filter:
+                    self.start_date.append(row['start_date'])
+                    self.project.append(row['project'])
+                    self.booking_status.append(row['booking_status'])
+                    self.booker.append(row['booker'])
+                    self.owner.append(row['owner'])
+                    self.project_type.append(row['project_type'])
+                    self.booking_type.append(row['booking_type'])
+                    self.repeat_type.append(row['repeat_type'])
+                    self.finish_date.append(row['finish_date'])
+                    self.duration_minutes.append(row['duration_minutes']) # str
+                    tmp_duration_hours.append(float(row['duration_minutes'])/60.0) #list
+                    self.booking_description.append(row['booking_description'])
+                    self.cancellation_date.append(row['cancellation_date'])
+                    self.cancellation_notice_length_days.append(row['cancellation_notice_length_days'])
+                    self.cancellation_notice_length_verbose.append(row['cancellation_notice_length_verbose'])
+                    self.cancellation_reason.append(row['project'])
+                    self.cancellation_comments.append(row['project'])
         self.duration_hours = np.array(tmp_duration_hours)
+        self.calc_booking_date()
 
     def calc_booking_date(self):
         # take date part and split y, m, d
         week_num = [] # as list
         for ii in self.start_date:
             date_tmp = ii[0:10]
-            print date_tmp
             if '-' in date_tmp:  # yyyy-mm-dd format
-                print "yyyy-mm-dd format"
                 date_split = date_tmp.split('-') # date string
             elif '/' in date_tmp: # dd/mm/yyy format
-                print "dd/mm/yyyy format"
                 date_split_rev = date_tmp.split('/')
                 date_split = date_split_rev[::-1] #reverse
             else:
-                print "dunno" 
+                print "Unrecognised date format." 
             time_tmp = ii[11:]
             time_split = time_tmp.split(':') 
-            if len(time_split) == 2:  # handle missing ss in hh:mm format
+            if len(time_split) == 3:
+                pass
+            elif len(time_split) == 2:  # handle missing ss in hh:mm format
                 time_split.append('0')
+            else:
+                print "Unrecognised time format."
             # date_split needs to be [yyyy, mm, dd]
             datetime_tmp = datetime.datetime( int(date_split[0]), int(date_split[1]), int(date_split[2]), \
                                           int(time_split[0]), int(time_split[1]), int(time_split[2]) )    
@@ -105,17 +108,19 @@ class Resource:
     def calc_bookings_weeknum(self):
         # use lowest and highest week numbers in the data
         week_num = range(min(self.booking_week_num), max(self.booking_week_num)+1)
-        booking_week_hours_true = []
+        booking_week_hours = []
+        booking_week_approved_hours = []
         week_hours = []
+        week_approved_hours = []
         for wk in week_num:
             # include hours if booking is a member of week_num
-            booking_week_hours_true = np.where(self.booking_week_num == wk, \
+            booking_week_hours = np.where(self.booking_week_num == wk, \
                                                self.duration_hours, 0)
-            week_hours.append(np.sum(booking_week_hours_true))
+            week_hours.append(np.sum(booking_week_hours))
         return (week_num, week_hours)
         
     def debug_num_fields(self):
-        num_fields = [len(self.resource_name), \
+        num_fields = [len(self.resource_name_filter), \
                       len(self.resource),\
                       len(self.project),\
                       len(self.booking_status),\
@@ -140,18 +145,47 @@ class Resource:
         print("debug_num_fields " + str(num_fields))
 
 #currently local.  should be a new class for summary data?
-def show_bookings(date_axis, duration):
+def show_bookings_approved_cancelled(date_axis1, duration1, date_axis2, duration2, resource):
     fig1 = plt.figure()
     ax1 = fig1.add_subplot(1,1,1)
-    ax1.plot(date_axis , duration )
-    plt.show()        
+    ax1.plot(date_axis1 , duration1, date_axis2, duration2 )
+    plt.title(resource)
+    ax1.set_label(['Approved','Cancelled'])
+    plt.legend()
+    plt.show(block=True)        
 
-dec_bookings = Booking(fname)
+dec_bookings = BookingSource(fname)
 dec_bookings.read_csv()
 
-prisma_west = Resource('3TW')
-prisma_west.get_bookings(dec_bookings.booking_dict)
-prisma_west.calc_booking_date()
-prisma_west.debug_num_fields()
-(wk, hrs) = prisma_west.calc_bookings_weeknum()
-show_bookings(wk, hrs)
+scanner_list = ['3TW', '3TE', '7T', '3TM' ]
+booking_status = ['APPROVED','CANCELLED']
+
+hours = {}   # empty dict
+week_axis = {}   # empty dict
+
+for scanner in scanner_list:
+    # hours is a dict where the top level key is the scanner type.  Each value is itself a dict
+    # with keys APPROVED and CANCELLED, and the value associated with APPROVED or CANCELLED
+    # are the hours
+    hours[scanner] = {} # define each 3TE, 3TW, ... key as a dict
+    week_axis[scanner]={}
+    for status in booking_status:
+        scanner_hours = BookingFilter(scanner, status)
+        scanner_hours.get_bookings(dec_bookings.booking_dict)
+        (wk, hrs) = scanner_hours.calc_bookings_weeknum()
+        # populate a dict with the form hours[scanner][status] = [... list of hours values ...]
+        hours[scanner][status] = hrs #define the booking_status key and populate values from list hrs.
+        week_axis[scanner][status] = wk    
+    show_bookings_approved_cancelled(week_axis[scanner]['APPROVED'], hours[scanner]['APPROVED'], \
+                                     week_axis[scanner]['CANCELLED'], hours[scanner]['CANCELLED'], \
+                                     [scanner])
+
+##prisma_west_approved = BookingFilter('3TW', 'APPROVED')
+##prisma_west_approved.get_bookings(dec_bookings.booking_dict)
+##(wk1, hrs_app) = prisma_west_approved.calc_bookings_weeknum()
+##
+##prisma_west_cancelled = BookingFilter('3TW', 'CANCELLED')
+##prisma_west_cancelled.get_bookings(dec_bookings.booking_dict)
+##(wk2, hrs_can) = prisma_west_cancelled.calc_bookings_weeknum()
+##
+##show_bookings_approved_cancelled(wk1, hrs_app, wk2, hrs_can, '3TW')
