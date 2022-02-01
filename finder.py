5720235cb5d624e9d7cca0ebb580c85c1b462f35
John Evans <cjohnevans@users.noreply.github.com>
2022-02-01 11:30:19 +0000

    Debug version of resource availability implemented

--- finder.py
+++ finder.py
@@ -5,31 +5,29 @@ import numpy as np
 
 ###   BookingFinder #################################################################
 class BookingFinder:
-    def load_template(self, filename):
+    def load_availability(self, filename):
         '''
-        input:  filename of cvs in the format given by booking_template.csv
+        input:  filename of cvs in the format given by finder_template.csv
                 assumes a format of 3TE,3TE,3TM,7T,operator with an (ignored) header
         output: numpy array with booking template        
         '''
 
-        template = [] #as list
+        availability = [] #as list
         
         with open(filename, 'r') as csvfile:
             header = csvfile.readline().split(',')
             header = [header[idx].strip() for idx in range(0,len(header)) ]
 
             print('filename: ' + filename)
-            print(header)
 
             csvlines = csv.reader(csvfile, delimiter=',')
             for row in csvlines:
-                template_line = [ int(resource.strip()) for resource in row ]
-                template.append(template_line)
-        print(template)
-        self.template_np = np.array(template)
-        print(self.template_np)
+                availability_line = [ int(resource.strip()) for resource in row ]
+                availability.append(availability_line)
+        availability_np = np.array(availability)
+        return availability_np
      
-    def load_template_dict(self, filename):
+    def load_availability_dict(self, filename):
         '''
         input:  filename of cvs in the format given by booking_template.csv
                 loads template as dict
@@ -70,13 +68,21 @@ class BookingFinder:
         #print(test)
         #print(type(test[0][0]))
 
-    def calc_availability(self):
+    def get_availability(self, is_debug):
         '''
         input: a BookingFilter object.  Specifically needs booking_datetime (start time),
           booking duration_hours (numpy array)
+               is_debug boolean.  if true load from file rather than calculate.
         output: numpy array of availabilities
         '''
-        return
+        if is_debug:
+            print('debug')
+            resource_availability = self.load_availability('debug_availability.csv')
+        else:
+            # get resource_availability from BookingFinder calculation
+            pass
+
+        return resource_availability
 
     def find_slot(self):
         '''
@@ -84,12 +90,17 @@ class BookingFinder:
         and works out the available slots
         output: list of slots
         '''
-        return 0
+        
+        return 
 
-    def empty_fn(self):
-        return "Hello from BookingFinder"
+
 
 ###   main() #################################################################
 test_finder = BookingFinder()
-test_finder.load_template('finder_template.csv')
-print(test_finder.empty_fn())
+template = test_finder.load_availability('finder_template.csv')
+print(template)
+resource = test_finder.get_availability(True)
+#need some error trapping here
+print(resource)
+
+
