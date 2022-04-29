808c3423d818e4e2be4189d26e0aeaa3a05e1ea4
sapje1 <evansj31@cardiff.ac.uk>
2022-04-29 13:22:28 +0100

    changed working directory, updating plot code for 2022 data

--- mrusage.py
+++ mrusage.py
@@ -7,12 +7,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import finder
 import calpendobookings as cal
+import os
 
-#fname = 'mri_activity_dec_2021.csv'
-#fname = 'mri_activity_aprnov_2021.csv'
+home_dir = os.environ['HOME']
+data_dir = os.path.join(home_dir, 'data_sapje1/mr_usage')
+print(data_dir)
+
 # this is the utf-8 encoded version:
-fname = 'mri_activity_2021aprdec_all_b.csv'
+fname = 'mri_activity_2021aprdec_all_b.csv'  # 2021 data
+fname = 'mri_activity_2022.csv'  #2022 data
 
+file_path = os.path.join(data_dir, fname)
         
 #### main() ##############################################################################
 #  this spans multiple resources, across multiple classes, so doesn't fit in BookingAnalyse...
@@ -33,15 +38,18 @@ def bookings_stacked_bar(x_data, y_data, figfname, title):
     fig, ax = plt.subplots()
     ax.set_ylim(0, hours_max)
     for series in x_data.keys():
+        print(len(x_data[series]))  # BUG need to deal with series of different lengths.
         ax.bar(x_data[series] , y_data[series], bottom = start_height)
         # work out starting point for next bar series, by list comprehension.
         start_height = [start_height[jj] + y_data[series][jj] for jj in range(len(start_height))]
     plt.legend(x_data.keys())
     plt.title(title)
 #    plt.show(block=False)
-    plt.savefig('output/' + figfname)
+    fig_path = os.path.join(data_dir, 'output', figfname)
+    print(fig_path)
+    plt.savefig(fig_path)
 
-cal_bookings = cal.BookingSource(fname)
+cal_bookings = cal.BookingSource(file_path)
 
 #resource_list = ['3TE', '3TW', '7T', '3TM', 'Peter Hobden', 'Allison Cooper', 'Sonya Foley', 'John Evans'  ]
 scanner_list = ['3TE', '3TW', '7T', '3TM']
@@ -72,21 +80,21 @@ scanner_stacked_hours = { resource: booking_analysis_approved[resource].week_hou
 bookings_stacked_bar(scanner_stacked_axes, scanner_stacked_hours,\
                      'ScannerHours.png','Hours booked per week, by scanner')
 
-operator_booking = {}
-operator_analysis = {}
-for resource in operator_list:
-    # calls get_bookings function from the resource-specific instance of the BookingFilter class
-    # booking_list_approved.
-    operator_booking[resource] = cal.BookingFilter(cal_bookings.booking, resource, 'APPROVED')
-    operator_analysis[resource] = cal.BookingAnalyse(resource)
-    operator_analysis[resource].calc_bookings_weeknum(operator_booking[resource])
-    operator_analysis[resource].plot_hours()
-
-operator_stacked_axes = { resource: operator_analysis[resource].week_num for resource in operator_list }
-
-operator_stacked_hours = { resource: operator_analysis[resource].week_hours for resource in operator_list }
-bookings_stacked_bar(operator_stacked_axes, operator_stacked_hours, \
-                     'OperatorHours.png', 'Hours booked per week, by operator')
+##operator_booking = {}
+##operator_analysis = {}
+##for resource in operator_list:
+##    # calls get_bookings function from the resource-specific instance of the BookingFilter class
+##    # booking_list_approved.
+##    operator_booking[resource] = cal.BookingFilter(cal_bookings.booking, resource, 'APPROVED')
+##    operator_analysis[resource] = cal.BookingAnalyse(resource)
+##    operator_analysis[resource].calc_bookings_weeknum(operator_booking[resource])
+##    operator_analysis[resource].plot_hours()
+##
+##operator_stacked_axes = { resource: operator_analysis[resource].week_num for resource in operator_list }
+##
+##operator_stacked_hours = { resource: operator_analysis[resource].week_hours for resource in operator_list }
+##bookings_stacked_bar(operator_stacked_axes, operator_stacked_hours, \
+##                     'OperatorHours.png', 'Hours booked per week, by operator')
 
 bk_7t = cal.BookingFilter(cal_bookings.booking, '7T', 'APPROVED')
 #an_7t = cal.BookingAnalyse('7T')
